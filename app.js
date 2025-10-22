b5235a8171038cd0915cde6db165853cda215f27
Sweta Dahal <dahalsweta5@gmail.com>
2025-10-22 16:04:18 +0545

    Fix serverless configuration

--- app.js
+++ app.js
@@ -65,9 +65,10 @@ app.use((req, res) => {
     });
 });
 
-app.listen(3000,()=>{
-    console.log("Server started at http://localhost:3000");
-    
-})
+if(require.main === module){    
+    app.listen(3000,()=>{
+        console.log("Server started at http://localhost:3000");    
+    });
+}
 
 module.exports = app;
